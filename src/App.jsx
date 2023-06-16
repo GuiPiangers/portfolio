b75f11f09d2cd7a94e63d21f80b378662562ffd1
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-15 23:32:12 -0300

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Components/Home/Hero/Hero'
 import About from './Components/Home/About/About'
 import MyProjects from './Components/Home/MyProjects/MyProjects'
 import Contact from './Components/Home/Contact/Contact'
+import Footer from './Components/Layout/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About/>
       <MyProjects/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,33 @@
+import './Footer.css'
+import BoxContainer from '../Templates/BoxContainer/BoxContainer'
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <h2 className='footer__logo'>Home</h2>
+            <BoxContainer className='flex-row'>
+                <div className='footer__container'>
+                    <h2 className='footer__title'>Projetos</h2>
+                    <div className='footer__item'>
+                        <p>Sistema de Barbearia</p>
+                    </div>
+                    <div className='footer__item'>
+                        <p>ToDo List</p>
+                    </div>
+                </div>
+                <div className='footer__container'>
+                    <h2 className='footer__title'>Contatos</h2>
+                    <div className='footer__item'>
+                        <p>(51) 9 8035-1927</p>
+                    </div>
+                    <div className='footer__item'>
+                        <p>eduardomartinspiangers@gmail.com</p>
+                    </div>
+                    <div className='footer__item'>
+                        <p>gui-piangers</p>
+                    </div>
+                </div>
+            </BoxContainer>
+        </footer>
+    )
+}
